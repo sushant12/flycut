1d02a4c6764854e7bc10f9c2cecae1ac1da4c4a1
sushant12 <sussyoung9@gmail.com>
2019-10-12 11:05:06 +0545

    set opacity

--- main.cpp
+++ main.cpp
@@ -8,13 +8,15 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+//    hide menu bar
     w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+//    set opacity
+    w.setProperty("windowOpacity", 0.8);
+//    center the main window
     QRect screenGeometry = QApplication::desktop()->screenGeometry();
     int x = (screenGeometry.width()-w.width()) / 2;
     int y = (screenGeometry.height()-w.height()) / 2;
     w.move(x, y);
-    w.show();
-//    QGuiApplication::screens
     w.show();
     return a.exec();
 }
